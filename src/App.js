ea6cf9729f9a85eb1a3b51382a4718783a40419a
Dinika Saxena <dinika.saxena@cern.ch>
2018-05-04 15:13:03 +0200

    initial commit
    Signed-off-by: Dinika Saxena <dinika.saxena@cern.ch>

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import Person from './Person/Person';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <h1>Exceptions Dashboard</h1>
+        <Person name="DoJSON Errors" total="20"/>
+        <Person name="Validation Errors" total="34"/>
+        <Person name="PID Errors" total="2"/>
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ src/Person/Person.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const person = (props) => {
+    return <p>Total number of {props.name} errors {props.total} </p>
+}
+
+export default person;
