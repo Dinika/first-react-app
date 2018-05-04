eee58092f69caf4db3350e66c21503b36a994d33
Dinika Saxena <dinika.saxena@cern.ch>
2018-05-04 16:12:34 +0200

    dynamically add content to components
    Signed-off-by: Dinika Saxena <dinika.saxena@cern.ch>

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ class App extends Component {
         <h1>Exceptions Dashboard</h1>
         <Person name="DoJSON Errors" total="20"/>
         <Person name="Validation Errors" total="34"/>
-        <Person name="PID Errors" total="2"/>
+        <Person name="PID Errors" total="2"> Data still to be added </Person>
       </div>
     );
   }
--- src/Person/Person.js
+++ src/Person/Person.js
@@ -1,7 +1,13 @@
 import React from 'react';
 
 const person = (props) => {
-    return <p>Total number of {props.name} errors {props.total} </p>
+    return (
+        <div>
+            <p>Total number of {props.name} errors {props.total} </p>
+            <p>{props.children}</p>
+        </div>
+
+    )
 }
 
 export default person;
