e3ac94ffc30438a8f75e2006ae0cef5a468d145f
Dinika Saxena <dinika@localhost.localdomain>
2018-05-06 10:57:16 +0200

    set two way binding
    Signed-off-by: Dinika Saxena <dinika@localhost.localdomain>

--- src/App.js
+++ src/App.js
@@ -11,11 +11,21 @@ class App extends Component {
     ]
   }
 
-  switchNameHandler = () => {
+  switchQuantityHandler = (newQuanity) => {
     this.setState({
       errors: [
-        {type: 'Validation', quantity: 34},
+        {type: 'Validation', quantity: newQuanity},
         {type: 'DoJSON', quantity: 12},
+        {type: 'PID Type', quantity: 2},
+      ]
+    })
+  }
+
+  typeChangeHandler = (event) => {
+    this.setState({
+      errors: [
+        {type: 'Validation', quantity: 24},
+        {type: event.target.value , quantity: 12},
         {type: 'PID Type', quantity: 2},
       ]
     })
@@ -25,10 +35,18 @@ class App extends Component {
     return (
       <div className="App">
         <h1>Exceptions Dashboard</h1>
-        <button onClick={this.switchNameHandler}>Reload</button>
-        <Error type = {this.state.errors[0].type} quantity = {this.state.errors[0].quantity}/>
-        <Error type = {this.state.errors[1].type} quantity = {this.state.errors[1].quantity}/>
-        <Error type = {this.state.errors[2].type} quantity = {this.state.errors[2].quantity}> Data still to be added </Error>
+        <button onClick={this.switchQuantityHandler.bind(this, 20)}>Reload</button>
+        <Error 
+          type = {this.state.errors[0].type}
+          quantity = {this.state.errors[0].quantity}
+          click = {this.switchQuantityHandler.bind(this, 10)}/>
+        <Error
+          type = {this.state.errors[1].type} 
+          quantity = {this.state.errors[1].quantity}
+          change= {this.typeChangeHandler}/>
+        <Error 
+          type = {this.state.errors[2].type} 
+          quantity = {this.state.errors[2].quantity}> Data still to be added </Error>
       </div>
     );
   }
--- src/Error/Error.js
+++ src/Error/Error.js
@@ -3,8 +3,9 @@ import React from 'react';
 const error = (props) => {
     return (
         <div>
-            <p>Total number of {props.type} errors {props.quantity} </p>
+            <p onClick={props.click}>Total number of {props.type} errors {props.quantity} </p>
             <p>{props.children}</p>
+            <input type="text" onChange={props.change} value={props.type}/>
         </div>
 
     )
