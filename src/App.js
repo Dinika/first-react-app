3c4f375843e18c5998a08d1a64bfc08cd9d0a3c6
Dinika Saxena <dinika.saxena@cern.ch>
2018-05-11 16:17:36 +0200

    add styles dynamically
    Signed-off-by: Dinika Saxena <dinika.saxena@cern.ch>

--- src/App.js
+++ src/App.js
@@ -1,14 +1,18 @@
 import React, { Component } from 'react';
 import Error from './Error/Error';
 import './App.css';
+import data from './report-errors.json';
 
 class App extends Component {
   state = {
+    exceptions: data,
     errors: [
-      {type: 'Validation', quantity: 24},
-      {type: 'DoJSON', quantity: 42},
-      {type: 'PID Type', quantity: 2},
+      {id: '123', type: 'Validation', quantity: 22},
+      {id: '345', type: 'DoJSON', quantity: 42},
+      {id: '725', type: 'PID Type', quantity: 2},
     ],
+    typeOfErrors: [],
+    collections: [],
     showErrors : false,
   }
 
@@ -22,14 +26,16 @@ class App extends Component {
     })
   }
 
-  typeChangeHandler = (event) => {
-    this.setState({
-      errors: [
-        {type: 'Validation', quantity: 24},
-        {type: event.target.value , quantity: 12},
-        {type: 'PID Type', quantity: 2},
-      ]
-    })
+  typeChangeHandler = (event, id) => {
+    const errorIndex = this.state.errors.findIndex((error) => {
+      return error.id === id
+    });
+  
+    const errorToUpdate = { ...this.state.errors[errorIndex]}
+    errorToUpdate.type = event.target.value;
+    const errors = [...this.state.errors];
+    errors[errorIndex] = errorToUpdate;
+    this.setState({errors: errors})
   }
 
   toggleErrorsHandler = () => {
@@ -37,6 +43,13 @@ class App extends Component {
     this.setState({
       showErrors: !doesShow
     });
+  }
+
+  updateErrorCounterHandler = () => {
+    console.log(this.state.exceptions.reduce( (tally, exception) => {
+        tally[exception.dojson] = (tally[exception.dojson] || 0) + 1 ;
+        return tally
+    }, {}))
   }
 
   deleteErrorHandler = (errorIndex) => {
@@ -54,25 +67,41 @@ class App extends Component {
       cursor: 'pointer'
     }
 
-    let persons = null;
+    let exceptionsMetadata = null;
+
+    const classes = [];
+    if(this.state.errors.length <= 1) {
+      classes.push('red');
+    }
+    if(this.state.errors.length <= 2) {
+      classes.push('bold');
+    }
 
     if(this.state.showErrors) {
-      persons = (
+      exceptionsMetadata = (
         <div>
           {this.state.errors.map( (error, index) => {
-            return <Error type = {error.type}
+            return <Error 
+                  key = {error.id}
+                  type = {error.type}
                   quantity = {error.quantity}
-                  click = {this.deleteErrorHandler.bind(this, index)}/>
+                  click = {this.deleteErrorHandler.bind(this, index)}
+                  change = {(event) => {this.typeChangeHandler(event, error.id)}}/>
           })}
         </div>
       );
+      style.backgroundColor = 'red';
     }
+
 
     return (
       <div className="App">
         <h1>Exceptions Dashboard</h1>
+        <p className={classes.join(' ')}>Click the buttons to see metadata</p>
         <button style={style} onClick={this.toggleErrorsHandler}>Show Erros</button>
-        {persons} 
+        <button style={style} onClick={this.updateErrorCounterHandler}>Console errors</button>
+        {exceptionsMetadata}
+        
       </div>
     );
   }
